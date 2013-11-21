7526f2d8b097a9268ee3822e489f548a0406661d
georgecook <georgecook@gmail.com>
2013-11-21 13:14:20 -0800

    changing logging

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var sys = require("sys"),
 my_http = require("http");
 my_http.createServer(function(request,response){
-	sys.puts("I got kicked");
+	console.log("I got kicked");
 	response.writeHeader(200, {"Content-Type": "text/plain"});
-	response.write("Hey now, World");
+	response.write("Hey now, World redux");
 	response.end();
 }).listen(80);
-sys.puts("Server Running on 80");	
+console.log("Server Running on 80");	
